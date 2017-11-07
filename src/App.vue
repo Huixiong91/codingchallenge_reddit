ca27ebb80cb5b32c542855f5cc303b6d79cad6b6
HuiXiong(Windows) <Huixiong9191@gmail.com>
2017-11-07 22:37:05 +0800

    Add initial post component with the data structure. Proof-Of-Concept by display posts with some dummy post data first.
    TODO: Can improve on visual presentation. Include a dialog/form for user
    to create new posts.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <v-app>
+    <main>
+      <v-content>
+        <post v-for="post in posts" :key="post.title" :post="post" v-on:voted="voted" >
+        </post>
+      </v-content>
+    </main>
     <v-footer app>
       <span>&copy; 2017</span>
     </v-footer>
@@ -7,11 +13,50 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-      }
+import Post from './components/post'
+
+export default {
+  components: {
+    'post': Post
+  },
+  methods: {
+    voted: function () {
+      this.posts.sort(function (a, b) {
+        let netVotesA = a.numOfUpVotes - a.numOfDownVotes
+        let netVotesB = b.numOfUpVotes - b.numOfDownVotes
+        if (netVotesA > netVotesB) return -1
+        else if (netVotesA < netVotesB) return 1
+        return 0
+      })
+    }
+  },
+  data () {
+    return {
+      posts: [
+        {
+          title: 'Post #1',
+          numOfDownVotes: 0,
+          numOfUpVotes: 0
+        },
+        {
+          title: 'Post #2',
+          numOfDownVotes: 0,
+          numOfUpVotes: 0
+        },
+        {
+          title: 'Post #3',
+          numOfDownVotes: 0,
+          numOfUpVotes: 0
+        },
+        {
+          title: 'Post #4',
+          numOfDownVotes: 0,
+          numOfUpVotes: 0
+        }
+      ]
     }
   }
-
+}
 </script>
+
+
---
+++ src/components/Post.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card style="margin:10px">
+      {{ post.title }}
+      {{ post.numOfUpVotes}}
+      {{ post.numOfDownVotes}}
+      {{ netVotes }}
+      <v-btn @click="upVote">
+        Up
+      </v-btn>
+      <v-btn @click="downVote">
+        Down
+      </v-btn>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['post'],
+  methods: {
+    downVote: function () {
+      this.post.numOfDownVotes++
+      this.$emit('voted')
+    },
+    upVote: function () {
+      this.post.numOfUpVotes++
+      this.$emit('voted')
+    }
+  },
+  computed: {
+    netVotes: function () {
+      return this.post.numOfUpVotes - this.post.numOfDownVotes
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+</style>
