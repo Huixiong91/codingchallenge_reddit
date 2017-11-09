131c7c4927ab657ba1e85e6cb6636305953e8dea
HuiXiong(Windows) <huixiong9191@gmail.com>
2017-11-09 16:58:50 +0800

    Better layout for post

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,15 +1,27 @@
 <template>
   <v-card style="margin:10px">
-      {{ post.title }}
-      {{ post.numOfUpVotes}}
-      {{ post.numOfDownVotes}}
-      {{ netVotes }}
-      <v-btn id="upvoteBtn" @click="upVote">
-        Up
-      </v-btn>
-      <v-btn id="downvoteBtn" @click="downVote">
-        Down
-      </v-btn>
+    <v-layout>
+      <v-flex xs1 style="margin:5px">
+        <v-layout column >
+          <v-flex  style="align-self:center">
+            <v-btn small id="upvoteBtn" @click="upVote" flat icon>
+              <v-icon>keyboard_arrow_up</v-icon>
+            </v-btn>
+          </v-flex>
+          <v-flex v-if="post.numOfUpVotes || post.numOfDownVotes">
+              <h6 style="text-align:center;margin:0px">{{ netVotes }}</h6>
+          </v-flex>
+          <v-flex style="align-self:center">
+            <v-btn small id="downvoteBtn" @click="downVote" flat icon>
+              <v-icon>keyboard_arrow_down</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex xs11 style="margin:5px">
+      <h5>{{ post.title }}</h5>
+      </v-flex>
+    </v-layout>
   </v-card>
 </template>
 
